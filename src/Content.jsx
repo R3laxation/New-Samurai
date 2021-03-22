b0cbed5e67268924c3af441e5acd577cc6fbbfc1
Konstantin <memories@bk.ru>
2021-03-22 22:11:40 +0600

    First commit

---
+++ src/Content.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Content = () => {
+	return (
+		<div className='content'>
+			<img
+				src="https://www.passporthealthglobal.com/wp-content/uploads/2020/06/vaccines-advice-sweden-img-compressed.jpg"
+				alt="bg"/>
+			<div>ava+descr</div>
+			<div>My Posts
+				<div>New Post</div>
+				<div>Post1</div>
+				<div>Post2</div>
+				<div>Post3</div>
+			</div>
+		</div>
+	)
+}
+
+export default Content;
